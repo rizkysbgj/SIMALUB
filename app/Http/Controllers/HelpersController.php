9a3897ccab76cb6a5c642caf0bdce4284970c5e7
rizkysbgj <rizkysubagja99@gmail.com>
2019-03-25 11:49:12 +0700

    template sertifikat

--- app/Http/Controllers/HelpersController.php
+++ app/Http/Controllers/HelpersController.php
@@ -6,6 +6,20 @@ use Illuminate\Http\Request;
 
 class HelpersController extends Controller
 {
+    public function exportsertifikat()
+    {
+        $phpWord = new \PhpOffice\PhpWord\PhpWord();
+        $template = $phpWord->loadTemplate(storage_path('template_sertifikat'));
+        // $template->setValue('test', 'Hello');
+
+        header('Content-Type: application/octet-stream');
+        header("Content-Disposition: attachment; filename=result.docx");
+        
+        $template->saveAs(storage_path('sertifikat.docx'));
+        readfile(storage_path('sertifikat.docx'));
+        unlink(storage_path('sertifikat.docx'));
+    }
+
     public function exportdocx($templatename)
     {
         // $phpWord = new \PhpOffice\PhpWord\PhpWord();
