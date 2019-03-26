058adb5e17abd9e21c3c63e4f40a3d0bc3a9f068
rasyidrio <[rioalrasyid97@gmail.com]>
2019-03-26 11:34:57 +0700

    ulasan dari customer di dashboard MT

--- resources/views/manajerTeknis/layout/page/dashboardMT/detailDashboardMT.blade.php
+++ resources/views/manajerTeknis/layout/page/dashboardMT/detailDashboardMT.blade.php
@@ -213,6 +213,137 @@
         </div>
     </div>
 
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="m-portlet m-portlet--mobile">
+                <div class="m-portlet__head">
+                    <div class="m-portlet__head-caption">
+                        <div class="m-portlet__head-title">
+                            <h3 class="m-portlet__head-text">
+                                Uraian Proyek dari Customer
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="m-portlet__body">
+                    <!--begin: Datatable -->
+                    <div class="m-datatable m-datatable--default m-datatable--brand m-datatable--loaded">
+                        <table class="m-datatable__table" id="html_table" width="100%" style="display: block; min-height: 300px; overflow-x: auto;">
+                            <thead class="m-datatable__head">
+                                <tr class="m-datatable__row" style="left: 0px;">
+                                    <th title="Field #1" class="m-datatable__cell m-datatable__cell--sort" style="width:300px;" data-field="Uraian"><span>Uraian</span></th>
+                                    <th title="Field #2" class="m-datatable__cell m-datatable__cell--sort" data-field="SM" style="width:184px;"><span style="text-align:center;">Sangat Memuaskan (5)</span></th>
+                                    <th title="Field #3" class="m-datatable__cell m-datatable__cell--sort" data-field="M" style="width:130px;"><span style="text-align:center;">Memuaskan (4)</span></th>
+                                    <th title="Field #4" class="m-datatable__cell m-datatable__cell--sort" data-field="N" style="width:85px;"><span style="text-align:center;">Netral (3)</span></th>
+                                    <th title="Field #5" class="m-datatable__cell m-datatable__cell--sort" data-field="KM" style="width:184px;"><span style="text-align:center;">Kurang Memuaskan (2)</span></th>
+                                    <th title="Field #6" class="m-datatable__cell m-datatable__cell--sort" data-field="SKM" style="width:230px;"><span style="text-align:center;">Sangat Kurang Memuaskan (1)</span></th>
+                                </tr>
+                            </thead>
+                            <tbody class="m-datatable__body" >
+                                <tr data-row="1" class="m-datatable__row" style="left: 0px;">
+                                <td data-field="Uraian" class="m-datatable__cell" style="background:#f4f3f8; width:300px;"><span >Independensi laboratorium dalam menghasilkan data pengujian</span></td>
+                                    <td data-field="SM" class="m-datatable__cell m--align-center" style="width:184px;"><span style="text-align:center;" class="Pertanyaan1-5"></span></td>
+                                    <td data-field="M" class="m-datatable__cell m--align-center" style="width:130px;"><span style="text-align:center;" class="Pertanyaan1-4"></span></td>
+                                    <td data-field="N" class="m-datatable__cell m--align-center" style="width:85px;"><span style="text-align:center;" class="Pertanyaan1-3"></span></td>
+                                    <td data-field="KM" class="m-datatable__cell m--align-center" style="width:184px;"><span style="text-align:center;" class="Pertanyaan1-2"></span></td>
+                                    <td data-field="SKM" class="m-datatable__cell m--align-center" style="width:230px;"><span style="text-align:center;" class="Pertanyaan1-1"></span></td>
+                                </tr>
+                                <tr data-row="2" class="m-datatable__row" style="left: 0px;">
+                                <td data-field="Uraian" class="m-datatable__cell" style="background:#f4f3f8; width:300px;"><span >Perlindungan atas kerahasiaan informasi</span></td>
+                                    <td data-field="SM" class="m-datatable__cell m--align-center" style="width:184px;"><span style="text-align:center;" class="Pertanyaan2-5"></span></td>
+                                    <td data-field="M" class="m-datatable__cell m--align-center" style="width:130px;"><span style="text-align:center;" class="Pertanyaan2-4"></span></td>
+                                    <td data-field="N" class="m-datatable__cell m--align-center" style="width:85px;"><span style="text-align:center;" class="Pertanyaan2-3"></span></td>
+                                    <td data-field="KM" class="m-datatable__cell m--align-center" style="width:184px;"><span style="text-align:center;" class="Pertanyaan2-2"></span></td>
+                                    <td data-field="SKM" class="m-datatable__cell m--align-center" style="width:230px;"><span style="text-align:center;" class="Pertanyaan2-1"></span></td>
+                                </tr>
+                                <tr data-row="3" class="m-datatable__row" style="left: 0px;">
+                                <td data-field="Uraian" class="m-datatable__cell" style="background:#f4f3f8; width:300px;"><span>Keakuratan laporan hasil uji</span></td>
+                                    <td data-field="SM" class="m-datatable__cell m--align-center" style="width:184px;"><span style="text-align:center;" class="Pertanyaan3-5"></span></td>
+                                    <td data-field="M" class="m-datatable__cell m--align-center" style="width:130px;"><span style="text-align:center;" class="Pertanyaan3-4"></span></td>
+                                    <td data-field="N" class="m-datatable__cell m--align-center" style="width:85px;"><span style="text-align:center;" class="Pertanyaan3-3"></span></td>
+                                    <td data-field="KM" class="m-datatable__cell m--align-center" style="width:184px;"><span style="text-align:center;" class="Pertanyaan3-2"></span></td>
+                                    <td data-field="SKM" class="m-datatable__cell m--align-center" style="width:230px;"><span style="text-align:center;" class="Pertanyaan3-1"></span></td>
+                                </tr>
+                                <tr data-row="4" class="m-datatable__row" style="left: 0px;">
+                                <td data-field="Uraian" class="m-datatable__cell" style="background:#f4f3f8; width:300px;"><span>Ketepatan waktu pengujian</span></td>
+                                    <td data-field="SM" class="m-datatable__cell m--align-center" style="width:184px;"><span style="text-align:center;" class="Pertanyaan4-5"></span></td>
+                                    <td data-field="M" class="m-datatable__cell m--align-center" style="width:130px;"><span style="text-align:center;" class="Pertanyaan4-4"></span></td>
+                                    <td data-field="N" class="m-datatable__cell m--align-center" style="width:85px;"><span style="text-align:center;" class="Pertanyaan4-3"></span></td>
+                                    <td data-field="KM" class="m-datatable__cell m--align-center" style="width:184px;"><span style="text-align:center;" class="Pertanyaan4-2"></span></td>
+                                    <td data-field="SKM" class="m-datatable__cell m--align-center" style="width:230px;"><span style="text-align:center;" class="Pertanyaan4-1"></span></td>
+                                </tr>
+                                <tr data-row="5" class="m-datatable__row" style="left: 0px;">
+                                <td data-field="Uraian" class="m-datatable__cell" style="background:#f4f3f8; width:300px;"><span>Komunikasi</span></td>
+                                    <td data-field="SM" class="m-datatable__cell m--align-center" style="width:184px;"><span style="text-align:center;" class="Pertanyaan5-5"></span></td>
+                                    <td data-field="M" class="m-datatable__cell m--align-center" style="width:130px;"><span style="text-align:center;" class="Pertanyaan5-4"></span></td>
+                                    <td data-field="N" class="m-datatable__cell m--align-center" style="width:85px;"><span style="text-align:center;" class="Pertanyaan5-3"></span></td>
+                                    <td data-field="KM" class="m-datatable__cell m--align-center" style="width:184px;"><span style="text-align:center;" class="Pertanyaan5-2"></span></td>
+                                    <td data-field="SKM" class="m-datatable__cell m--align-center" style="width:230px;"><span style="text-align:center;" class="Pertanyaan5-1"></span></td>
+                                </tr>
+                                <tr data-row="6" class="m-datatable__row" style="left: 0px;">
+                                <td data-field="Uraian" class="m-datatable__cell" style="background:#f4f3f8; width:300px;"><span>Tindakan lanjut dari pengaduan</span></td>
+                                    <td data-field="SM" class="m-datatable__cell m--align-center" style="width:184px;"><span style="text-align:center;" class="Pertanyaan6-5"></span></td>
+                                    <td data-field="M" class="m-datatable__cell m--align-center" style="width:130px;"><span style="text-align:center;" class="Pertanyaan6-4"></span></td>
+                                    <td data-field="N" class="m-datatable__cell m--align-center" style="width:85px;"><span style="text-align:center;" class="Pertanyaan6-3"></span></td>
+                                    <td data-field="KM" class="m-datatable__cell m--align-center" style="width:184px;"><span style="text-align:center;" class="Pertanyaan6-2"></span></td>
+                                    <td data-field="SKM" class="m-datatable__cell m--align-center" style="width:230px;"><span style="text-align:center;" class="Pertanyaan6-1"></span></td>
+                                </tr>
+                                <tr data-row="7" class="m-datatable__row" style="left: 0px;">
+                                <td data-field="Uraian" class="m-datatable__cell" style="background:#f4f3f8; width:300px;"><span>Fasilitas dan sumber daya manusia</span></td>
+                                    <td data-field="SM" class="m-datatable__cell m--align-center" style="width:184px;"><span style="text-align:center;" class="Pertanyaan7-5"></span></td>
+                                    <td data-field="M" class="m-datatable__cell m--align-center" style="width:130px;"><span style="text-align:center;" class="Pertanyaan7-4"></span></td>
+                                    <td data-field="N" class="m-datatable__cell m--align-center" style="width:85px;"><span style="text-align:center;" class="Pertanyaan7-3"></span></td>
+                                    <td data-field="KM" class="m-datatable__cell m--align-center" style="width:184px;"><span style="text-align:center;" class="Pertanyaan7-2"></span></td>
+                                    <td data-field="SKM" class="m-datatable__cell m--align-center" style="width:230px;"><span style="text-align:center;" class="Pertanyaan7-1"></span></td>
+                                </tr>
+                                <tr data-row="8" class="m-datatable__row" style="left: 0px;">
+                                    <td data-field="Uraian" class="m-datatable__cell" style="background:#f4f3f8; width:300px;"><span>Penerapan good profesional practice</span></td>
+                                    <td data-field="SM" class="m-datatable__cell m--align-center" style="width:184px;"><span style="text-align:center;" class="Pertanyaan8-5"></span></td>
+                                    <td data-field="M" class="m-datatable__cell m--align-center" style="width:130px;"><span style="text-align:center;" class="Pertanyaan8-4"></span></td>
+                                    <td data-field="N" class="m-datatable__cell m--align-center" style="width:85px;"><span style="text-align:center;" class="Pertanyaan8-3"></span></td>
+                                    <td data-field="KM" class="m-datatable__cell m--align-center" style="width:184px;"><span style="text-align:center;" class="Pertanyaan8-2"></span></td>
+                                    <td data-field="SKM" class="m-datatable__cell m--align-center" style="width:230px;"><span style="text-align:center;" class="Pertanyaan8-1"></span></td>
+                                </tr>
+                                <tr data-row="9" class="m-datatable__row" style="left: 0px;">
+                                    <td data-field="Uraian" class="m-datatable__cell" style="background:#f4f3f8; width:300px;"><span>Biaya pengujian</span></td>
+                                    <td data-field="SM" class="m-datatable__cell m--align-center" style="width:184px;"><span style="text-align:center;" class="Pertanyaan9-5"></span></td>
+                                    <td data-field="M" class="m-datatable__cell m--align-center" style="width:130px;"><span style="text-align:center;" class="Pertanyaan9-4"></span></td>
+                                    <td data-field="N" class="m-datatable__cell m--align-center" style="width:85px;"><span style="text-align:center;" class="Pertanyaan9-3"></span></td>
+                                    <td data-field="KM" class="m-datatable__cell m--align-center" style="width:184px;"><span style="text-align:center;" class="Pertanyaan9-2"></span></td>
+                                    <td data-field="SKM" class="m-datatable__cell m--align-center" style="width:230px;"><span style="text-align:center;" class="Pertanyaan9-1"></span></td>
+                                </tr>
+                                <!-- <span class="fa fa-check" style="color:#5867dd;"></span> -->
+                            </tbody>
+                        </table>
+                    <!--end: Datatable -->
+                    <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
+                        <div class="row align-items-center">
+                            <div class="col-xl-12">
+                                <div class="form-group m-form__group row align-items-center">
+                                    <div class="col-md-12">
+                                        <div class="m-form__group m-form__group--inline">
+                                            <h4>
+                                                <strong>Kritik dan Saran:</strong>
+                                            </h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-12">
+                                <div class="form-group m-form__group row align-items-center">
+                                    <div class="col-md-12">
+                                        <textarea type="text" class="form-control m-input" name="tbxKritikSaran" id="tbxKritikSaran" rows="6"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
 
 <script src="{{ asset('assets/app/js/dashboard.js') }}" type="text/javascript"></script>
 
